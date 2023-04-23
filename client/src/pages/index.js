4c09feb7b29628439f5a9edd8cb284ec27d1fbf5
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-04-23 22:10:55 +0600

    develop -- Add new species, add species from excel file

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,21 +1,13 @@
-import { Signup } from "./signup";
-import { Login } from "./login";
 // import Dashboard from "./dashboard/Dashboard";
 import ForgotPassword from "./forgot-password";
 import PasswordReset from "./password-reset";
-import Dashboard from "./dashboard";
-import { SpeciesList } from "./species";
 import { AddSpecies } from "./add-species";
 import EditSpecies from "./edit-species";
 import SpeciesDetails from "./species-details";
 
 export {
-  Signup,
-  Login,
   ForgotPassword,
   PasswordReset,
-  Dashboard,
-  SpeciesList,
   AddSpecies,
   EditSpecies,
   SpeciesDetails,
