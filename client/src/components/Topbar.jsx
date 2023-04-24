78298f3c5ec61c73c0c9ec1eb29775763a98e626
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-04-24 08:40:10 +0600

    fix ntopbar size and dashboard width

--- client/src/components/Topbar.jsx
+++ client/src/components/Topbar.jsx
@@ -4,31 +4,29 @@ import "../styles/topbar.css";
 
 function TopBar({ onToggleSidebar }) {
   return (
-    <div className="top-bar">
-      <nav>
-        <div className="container">
-          <img src={logo} className="logo" alt="" />
-          <div className="search-bar">
-            <span className="material-icons">search</span>
-            <input
-              type="search"
-              placeholder="Search Bird Species, Birding sites, Birders etc"
-            />
-          </div>
-          <div className="profile-area">
-            <div className="profile">
-              <div className="profile-photo">
-                <img src={profile} alt="profile" />
-              </div>
-              <h5>Karma Choden</h5>
-            </div>
-            <button id="menu-btn" onClick={onToggleSidebar}>
-              <span className="material-icons">menu</span>
-            </button>
-          </div>
+    <nav>
+      <div className="container">
+        <img src={logo} className="logo" alt="" />
+        <div className="search-bar">
+          <span className="material-icons">search</span>
+          <input
+            type="search"
+            placeholder="Search Bird Species, Birding sites, Birders etc"
+          />
         </div>
-      </nav>
-    </div>
+        <div className="profile-area">
+          <div className="profile">
+            <div className="profile-photo">
+              <img src={profile} alt="profile" />
+            </div>
+            <h5>Karma Choden</h5>
+          </div>
+          <button id="menu-btn" onClick={onToggleSidebar}>
+            <span className="material-icons">menu</span>
+          </button>
+        </div>
+      </div>
+    </nav>
   );
 }
 
