a509c73359ce0fe4285f1addf28fc9676f3f5882
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-05-04 10:03:58 +0600

    Responsive species and add species page, signup and login

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -22,8 +22,8 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const url = "http://localhost:8080/api/v1/users/register";
-      // const url = `${process.env.REACT_APP_API_URL}/api/v1/users/register`;
+      // const url = "http://localhost:8080/api/v1/users/register";
+      const url = `${process.env.REACT_APP_API_URL}/api/v1/users/register`;
 
       const { data: res } = await axios.post(url, data);
       navigate("/login");
@@ -178,63 +178,3 @@ const Signup = () => {
 };
 
 export default Signup;
-
-// edit
-
-// <div className={styles.signup_container}>
-//   <div className={styles.signup_form_container}>
-//     <div className={styles.left}>
-//       <h1>Welcome Back</h1>
-//       <Link to="/login">
-//         <button type="button" className={styles.white_btn}>
-//           Sign in
-//         </button>
-//       </Link>
-//     </div>
-//     <div className={styles.right}>
-//       <form className={styles.form_container} onSubmit={handleSubmit}>
-//         <h1>Create Account</h1>
-//         <input
-//           type="text"
-//           placeholder="First Name"
-//           name="firstName"
-//           onChange={handleChange}
-//           value={data.firstName}
-//           required
-//           className={styles.input}
-//         />
-//         <input
-//           type="text"
-//           placeholder="Last Name"
-//           name="lastName"
-//           onChange={handleChange}
-//           value={data.lastName}
-//           required
-//           className={styles.input}
-//         />
-//         <input
-//           type="email"
-//           placeholder="Email"
-//           name="email"
-//           onChange={handleChange}
-//           value={data.email}
-//           required
-//           className={styles.input}
-//         />
-//         <input
-//           type="password"
-//           placeholder="Password"
-//           name="password"
-//           onChange={handleChange}
-//           value={data.password}
-//           required
-//           className={styles.input}
-//         />
-//         {error && <div className={styles.error_msg}>{error}</div>}
-//         <button type="submit" className={styles.green_btn}>
-//           Sign Up
-//         </button>
-//       </form>
-//     </div>
-//   </div>
-// </div>;
