9b17507253e697f5a9e869a28ce7b0f8f98bf777
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-05-04 12:47:24 +0600

    update model name to Admin

--- server/passport.js
+++ server/passport.js
@@ -1,5 +1,5 @@
-// import * as dotenv from "dotenv";
-// dotenv.config();
+import * as dotenv from "dotenv";
+dotenv.config();
 
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import passport from "passport";
@@ -7,9 +7,8 @@ import passport from "passport";
 passport.use(
   new GoogleStrategy(
     {
-      clientID:
-        "320489601196-n64kjtci86p19g9okhpkf45jmsontqag.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: "/auth/google/callback",
       scope: ["profile", "email"],
     },
