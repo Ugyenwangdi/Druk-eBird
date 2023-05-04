8a1eba999a6b72515a49b21aef97bf3cb1903d25
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-05-04 11:41:28 +0600

    update model name to Admin

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,14 +1,19 @@
-import express from 'express';
+import express from "express";
 
-import { 
-  registerUser, loginUser, getAllUsers, getUserInfoByID,  
-} from '../controllers/user.controller.js';
+import {
+  registerUser,
+  loginUser,
+  getAllUsers,
+  getUserInfoByID,
+  deleteUser,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-router.route('/').get(getAllUsers);
-router.route('/register').post(registerUser);
-router.route('/login').post(loginUser);
-router.route('/:id').get(getUserInfoByID);
+router.route("/").get(getAllUsers);
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/:id").get(getUserInfoByID);
+router.route("/:id").delete(deleteUser);
 
 export default router;
