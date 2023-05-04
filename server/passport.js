acdbadedc0adc88d511ab5100941ebbade50b2f2
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-05-04 13:08:15 +0600

    update model name to Admin

--- server/passport.js
+++ server/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: process.env.CLIENT_URL,
       scope: ["profile", "email"],
     },
     function (accessToken, refreshToken, profile, callback) {
