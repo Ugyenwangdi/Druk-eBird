741a5e32c890eac75d3d831ae8c50c32038d4405
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-05-04 15:01:00 +0600

    update model name to Admin

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.CLIENT_URL,
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
