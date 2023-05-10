fa25c23f992676cb14b1fcdab5a783534c92a913
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-05-10 10:25:21 +0600

    fix loading bug

--- client/src/App.js
+++ client/src/App.js
@@ -83,6 +83,44 @@ function App() {
     setShowSidebar(!showSidebar);
   };
 
+  const handleCloseSidebar = () => {
+    setShowSidebar(false);
+  };
+  // adding an event listener to the window object to listen for changes in the screen size and update the state accordingly.
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setShowSidebar(true);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  // hidding the sidebar in mobile and tablet screen
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768) {
+        setShowSidebar(true);
+      } else {
+        setShowSidebar(false);
+      }
+    };
+
+    const mediaQuery = window.matchMedia("(min-width: 768px)");
+    setShowSidebar(mediaQuery.matches);
+
+    mediaQuery.addEventListener("change", handleResize);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleResize);
+    };
+  }, []);
+
   // render loading spinner/message while loading is true
   // if (loading) {
   //   return (
@@ -99,7 +137,8 @@ function App() {
           <Topbar onToggleSidebar={handleToggleSidebar} />
           <main>
             <Sidebar
-              show={showSidebar}
+              showSidebar={showSidebar}
+              closeSidebar={handleCloseSidebar}
               onToggleSidebar={handleToggleSidebar}
               style={{ position: "fixed" }}
             />
