a0d6cdc4413fd33a8a84690794aebcc6099c2679
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-05-20 06:52:18 +0600

    temp commit

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -41,7 +41,7 @@ function Sidebar({ showSidebar, closeSidebar }) {
       <aside
         className={`sidebar ${showSidebar ? "show" : ""}`}
         style={{
-          display: window.innerWidth >= 768|| showSidebar ? "block" : "none",
+          display: window.innerWidth >= 768 || showSidebar ? "block" : "none",
         }}
       >
         <button id="close-btn" onClick={closeSidebar} className="close-button">
