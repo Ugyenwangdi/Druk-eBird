6ca41d41e9de6731a95141ca422ac7448b7dfbfe
Ugyenwangdi <12190105.gcit@rub.edu.bt>
2023-06-05 21:47:48 +0600

    update topbar

--- client/src/pages/checklist.jsx
+++ client/src/pages/checklist.jsx
@@ -40,7 +40,7 @@ function Checklist() {
   };
 
   return (
-    <div className="checklists-page-container" >
+    <div className="checklists-page-container">
       <div
         style={{
           display: "flex",
@@ -129,7 +129,7 @@ function Checklist() {
         </div>
       </div>
 
-      <div className="checklist-table-container" >
+      <div className="checklist-table-container">
         {checklists.map((item, index) => {
           const serialNumber = (page - 1) * limit + index + 1;
           return (
